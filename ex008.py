660a23b37d06f08fe02761d282262d89346bafe1
deisedorea31 <deise.dorea31@gmail.com>
2025-09-24 09:20:33 -0300

    Correção

--- ex008.py
+++ ex008.py
@@ -1,31 +1,44 @@
-#Codificnado mensagens em Python - ZENIT POLAR
-
+# Codificando mensagens em Python - ZENIT POLAR (versão com replace)
 
 def zenit_polar_replace(text):
-    #Aplicar a codificação ZENIT POLAR utilizando o método replace
-    replacements = [('z', 'p'),('e', 'o'), ('n', 'l'), ('i','a'), ('t', 'r'),
-                   ('Z', 'P'), ('E', 'O'), ('N', 'L'), ('I', 'A'), ('T', 'R')]
+    # Substituições usando símbolos temporários para não "voltar" as trocas
+    replacements = [
+        ('z', '#'), ('e', '$'), ('n', '%'), ('i', '&'), ('t', '*'),
+        ('Z', '§'), ('E', '£'), ('N', '¢'), ('I', '¥'), ('T', '¤'),
+
+        ('p', 'z'), ('o', 'e'), ('l', 'n'), ('a', 'i'), ('r', 't'),
+        ('P', 'Z'), ('O', 'E'), ('L', 'N'), ('A', 'I'), ('R', 'T'),
+
+        ('#', 'p'), ('$','o'), ('%', 'l'), ('&','a'), ('*','r'),
+        ('§','P'), ('£','O'), ('¢','L'), ('¥','A'), ('¤','R')
+    ]
+
     for old, new in replacements:
         text = text.replace(old, new)
     return text
 
+
 def main():
-        #Entrada da frase e aplicação da codificação
-        frase = 'The quick brown fox jumps over the lazy dog'
-        frase = frase.title() #Primeira letra de cada palavra em maiúscula
+    # Frase inicial
+    frase = 'The quick brown fox jumps over the lazy dog'
+    frase = frase.title()  # Primeira letra de cada palavra em maiúscula
 
-        #Dividir a frase em palavras
-        words = frase.split()
+    # Dividir em palavras
+    words = frase.split()
 
-        #Processar cada palavra na lista usando ZENIT POLAR
-        coded_words = [zenit_polar_replace(word) for word in words]
+    # Aplicar a cifra em cada palavra
+    coded_words = [zenit_polar_replace(word) for word in words]
 
-        #Juntar todas as palavras codificadas em uma frase
-        coded_frase = " ".join(coded_words)
-        print('Original:', frase)
-        print('Coded:', coded_frase)
+    # Juntar com ESPAÇO entre as palavras
+    coded_frase = " ".join(coded_words)
+
+    print('Original:', frase)
+    print('Coded   :', coded_frase)
+
 
 if __name__ == "__main__":
     main()
+
+
 
 
